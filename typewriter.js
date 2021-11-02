79ba16e42db0995088cf54a3c1f210082321761d
keepitclassick <keepitclassick@gmail.com>
2021-11-02 14:39:39 -0400

    updated to call func

--- typewriter.js
+++ typewriter.js
@@ -1,6 +1,7 @@
 let sentence = "hello there from lighthouse labs";
 let delay = 500;
 
+const typewriter = function(sentence) {
 for (const letter of sentence) {
   setTimeout(() => {
     process.stdout.write(letter);
@@ -9,8 +10,9 @@ for (const letter of sentence) {
 } setTimeout(() => {
   process.stdout.write("\n");
 }, delay);
+}
 
-
+typewriter(sentence);
 //process.stdout.write removes the new line that is created at the end of a console.log
 
 
